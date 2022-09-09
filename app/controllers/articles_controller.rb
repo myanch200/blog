c31b1ae479ad25af9772832ae931d1aad72bb2ad
Martin Yanchev <martin@icecreamarchitecture.com>
2022-09-10 00:47:45 +0100

    adding update, using partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,20 @@ class ArticlesController < ApplicationController # rubocop:disable Style/Documen
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
 
