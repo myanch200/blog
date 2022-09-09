ca49c4c8314effd26a8526f3305dbf017e90fe68
Martin Yanchev <martin@icecreamarchitecture.com>
2022-09-10 00:31:14 +0100

    creating new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,33 @@
-class ArticlesController < ApplicationController
+# frozen_string_literal: true
+
+class ArticlesController < ApplicationController # rubocop:disable Style/Documentation
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
