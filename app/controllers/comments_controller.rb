e3a2c5e4801c9034e6139429271e1316adc6b641
Martin Yanchev <martin@icecreamarchitecture.com>
2022-09-10 01:30:46 +0100

    adding comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  before_action :set_article
+
+
+  def create
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
+end
